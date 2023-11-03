a8b3eb74432ceba03f9007a4b8e55cd1436752dc
unknown <naphatsawan.bw@gmail.com>
2023-11-04 03:19:53 +0700

    Test CI/CD Pipeline

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
   <NuxtLink to="/signup"> ลงทะเบียนที่นี่ </NuxtLink>
 
   <NuxtLink to="/login"> เข้าสู่ระบบ </NuxtLink>
+  <h1>Hello From Dear Test CI/CD kabbbb</h1>
 </template>
 <script setup lang="ts"></script>
 <style></style>
