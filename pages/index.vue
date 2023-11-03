b126c7fc99f2ef03808ee6d58d19dae827e365e6
unknown <naphatsawan.bw@gmail.com>
2023-11-04 03:40:09 +0700

    Test V.2

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <NuxtLink to="/signup"> ลงทะเบียนที่นี่ </NuxtLink>
 
   <NuxtLink to="/login"> เข้าสู่ระบบ </NuxtLink>
-  <h1>Hello From Dear Test CI/CD kabbbb</h1>
+  <h1>Hello From Dear Test CI/CD kabbbb V.2</h1>
 </template>
 <script setup lang="ts"></script>
 <style></style>
