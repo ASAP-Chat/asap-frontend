66fe4f79f8e0b2afec19f30fd783878aa9fdeab8
NAPHATSAWAN-DEAR <68803581+NAPHATSAWAN-DEAR@users.noreply.github.com>
2023-11-23 15:57:44 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  hooks: {
+    "pages:extend": (pages) => {
+      pages.map((page) => {
+        page.path = "/ssa3/" + page.path;
+      });
+    },
+  },
   ssr: false,
   devServer: {
     port: 4000,
