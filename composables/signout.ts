a01b824211766acbfa99e579f3c962d030cb344d
Thanyakorn Thalaengdi <68809500+pisenuta@users.noreply.github.com>
2023-12-12 12:37:37 +0700

    update: socket path

--- composables/signout.ts
+++ composables/signout.ts
@@ -5,9 +5,9 @@ import { Manager } from 'socket.io-client'
 // })
 // const socket = manager.socket('/latest-message')
 const manager = new Manager(import.meta.env.VITE_SOCKET_URL, {
-  path: '/ssa3/sockets/ws/'
+  path: '/ssa3/sockets/ws/',
 })
-const socket = manager.socket('/sockets/latest-message')
+const socket = manager.socket('/socketio/latest-message')
 
 export const useSignOut = () => {
   const accessToken = useCookie('accessToken')
