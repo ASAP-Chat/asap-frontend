54ae18d366f47cba23e3262d0d2eec5025ce6352
Thanyakorn Thalaengdi <68809500+pisenuta@users.noreply.github.com>
2024-01-22 03:45:06 +0700

    update: instagram url

--- composables/instagram.ts
+++ composables/instagram.ts
@@ -5,11 +5,9 @@ export const oauthInstagram = () => {
   if (process.env.NODE_ENV === 'development') {
     redirect_uri = 'http://localhost:4000/ssa3/setting/chat-integration/'
   } else {
-    redirect_uri =
-      'https://capstone23.sit.kmutt.ac.th/ssa3/setting/chat-integration/'
+    redirect_uri = 'https://capstone23.sit.kmutt.ac.th/ssa3/setting/chat-integration/'
   }
-  const scope =
-    'instagram_manage_messages,pages_manage_metadata,instagram_basic'
-  const url = `https://www.facebook.com/dialog/oauth?client_id=${appId}&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}`
+  const scope = 'instagram_manage_messages,pages_manage_metadata,instagram_basic'
+  const url = `https://www.facebook.com/dialog/oauth?client_id=${appId}&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=integrate-instagram`
   return url
 }
