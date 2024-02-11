c932dc4c5e4ac15148e77b90733b4130501c7db1
Thanyakorn Thalaengdi <68809500+pisenuta@users.noreply.github.com>
2024-02-12 00:40:37 +0700

    🦺 Update: middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,15 +1,16 @@
+import { Role } from '~/constants/Role'
 import { ACCESS_TOKEN, REFRESH_TOKEN, USER } from '~/constants/Token'
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const user: any = useCookie(USER)
   const access_token = useCookie(ACCESS_TOKEN)
   const refresh_token = useCookie(REFRESH_TOKEN)
-  const isOwner = user?.value?.isOwner
+  const role = user?.value?.role
 
   const allowedRoutes = ['/login/', '/signup/', '/', '/invite/']
   const chatRedirectRoutes = '/chat/'
   const profileRedirectRoute = '/setting/profile/'
-  const chatIntegrationRoute = '/setting/chat-integration/'
+  const settingRoute = ['/setting/chat-integration/', '/setting/member/']
 
   // Check if user is not authenticated
   if (!access_token.value || !refresh_token.value) {
@@ -24,7 +25,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
     }
 
     // Redirect non-owners trying to access chat integration to profile
-    if (user.value && !isOwner && to.path === chatIntegrationRoute) {
+    if (user.value && role === Role.AGENT && settingRoute.includes(to.path)) {
       return navigateTo(profileRedirectRoute)
     }
   }
