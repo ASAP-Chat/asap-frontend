54f22efe245bcc14b70fa0ef615079119e403d38
Thanyakorn Thalaengdi <68809500+pisenuta@users.noreply.github.com>
2024-02-16 11:28:45 +0700

    ✨ Update: profile page

--- pages/setting/profile.vue
+++ pages/setting/profile.vue
@@ -1,9 +1,20 @@
-<template></template>
+<template>
+  <div class="profile-info bg-white pa-5 tw-rounded-md">
+    <p class="mb-2"><b>ชื่อ :</b> {{ displayName }}</p>
+    <p class="mb-2"><b>อีเมล :</b> {{ email }}</p>
+    <p class="mb-2"><b>ตำแหน่ง :</b> {{ generateRole(role) }}</p>
+  </div>
+</template>
 <script setup lang="ts">
+import { USER } from '~/constants/Token'
+
 useHead({
   title: 'การตั้งค่า',
 })
 definePageMeta({
   layout: 'setting',
 })
+
+const user: any = useCookie(USER)
+const { email, displayName, role } = user.value && user.value
 </script>
