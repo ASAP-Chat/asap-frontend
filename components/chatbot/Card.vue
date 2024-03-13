8a2edce2e0435914be005dde02555762ffdbd334
Thanyakorn Thalaengdi <68809500+pisenuta@users.noreply.github.com>
2024-03-13 18:31:32 +0700

    💄 Update: chatbot ui

--- components/chatbot/Card.vue
+++ components/chatbot/Card.vue
@@ -1,37 +1,39 @@
 <template>
   <v-card
-    class="pa-5 mb-9"
+    class="pa-5 mb-9 tw-border tw-border-[#d4caff]"
     rounded="xl"
     flat
     :width="480"
   >
-    <div class="tw-text-end">
-      <v-btn
-        color="white"
-        density="compact"
-        flat
-        icon
-        @click="emit('edit')"
-      >
-        <v-icon color="primary">mdi-pencil-outline</v-icon>
-      </v-btn>
-    </div>
-    <div v-for="item in keyword">
-      <div class="tw-chat tw-chat-start tw-whitespace-pre-line">
-        <div class="tw-chat-bubble tw-bg-[#EEEEEE] text-black">{{ item }}</div>
+    <div class="tw-flex tw-flex-col tw-h-full">
+      <div class="tw-text-end">
+        <v-btn
+          color="white"
+          density="compact"
+          flat
+          icon
+          @click="emit('edit')"
+        >
+          <v-icon color="primary">mdi-pencil-outline</v-icon>
+        </v-btn>
       </div>
-    </div>
-
-    <div>
-      <div class="tw-chat tw-chat-end tw-whitespace-pre-line">
-        <div class="tw-chat-image">
-          <v-icon
-            size="x-large"
-            color="primary"
-            >mdi-robot-outline</v-icon
-          >
+      <div v-for="item in keyword">
+        <div class="tw-chat tw-chat-start tw-whitespace-pre-line">
+          <div class="tw-chat-bubble tw-bg-[#EEEEEE] text-black">{{ item }}</div>
         </div>
-        <div class="tw-chat-bubble tw-bg-[#d4caff] text-black">{{ replyMessage }}</div>
+      </div>
+
+      <div class="tw-mt-auto">
+        <div class="tw-chat tw-chat-end tw-whitespace-pre-line">
+          <div class="tw-chat-image">
+            <v-icon
+              size="x-large"
+              color="primary"
+              >mdi-robot-outline</v-icon
+            >
+          </div>
+          <div class="tw-chat-bubble tw-bg-[#d4caff] text-black">{{ replyMessage }}</div>
+        </div>
       </div>
     </div>
   </v-card>
